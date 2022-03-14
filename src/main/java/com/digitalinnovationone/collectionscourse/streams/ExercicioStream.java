62e5c5f3fd6950d5fa1f3edfa5e8b1236bbc514c
patriciaalencar <patricia.alencar@quikdev.com.br>
2022-03-14 20:55:55 -0300

    Streams

---
+++ src/main/java/com/digitalinnovationone/collectionscourse/streams/ExercicioStream.java
@@ -0,0 +1,17 @@
+package com.digitalinnovationone.collectionscourse.streams;
+
+/**
+ * Utilizando uma lista com um objeto complexo (Estudante, por exemplo) realize as seguintes operacoes:
+ * Sugestao em fazer em outras listas: queue, etc.
+ *
+ * A)Transforme cada estudante em uma string com os atributos do objeto
+ * B)Conte a quantidade de estudantes tem na colecao
+ * C)Filtre estudantes com idade igual ou superior a 18 anos
+ * D)Exiba cada elemento no console
+ * E)Retorne estudantes com nome que possui a letra B
+ * F)Retorne se existe ao menos um estudante com a letra D no nome
+ * G)Retorne o estudante mais velho (maior idade) da colecao. Retorne o mais novo tambem.
+ */
+public class ExercicioStream {
+
+}
